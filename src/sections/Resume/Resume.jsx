16b975a0cd140be0c202597f6ef78b07c6ab5b41
Karthik-Krishnano <73122483+Karthik-Krishnano@users.noreply.github.com>
2023-01-14 20:38:12 +0530

    .

--- src/sections/Resume/Resume.jsx
+++ src/sections/Resume/Resume.jsx
@@ -20,7 +20,68 @@ const Resume = () => {
           </div>
 
         <div class="row">
+         
+
+
+
+
           <div class="col-lg-6">
+            <h3 class="resume-title">Professional Experience</h3>
+            <div class="resume-item">
+
+            <a href="https://teamautoz.com/" role="button" rel="noreferrer" target="_blank">
+              <h4>Team AutoZ</h4>
+            </a>
+            
+              <h5>Head of Artificial Intelligence      (Aug 2022 - Present)
+              <br />Computer Vision Engineer      (Dec 2020 - Aug 2022)</h5>
+
+              <a href="https://goo.gl/maps/CC88KopimCAWEmV17" role="button" rel="noreferrer" target="_blank">
+              <p><i class="bi bi-geo-alt" />
+                <em>&nbsp;&nbsp;Vellore, Tamil Nadu </em></p>
+                </a>
+              <ul>
+                <li>Works on developing computer vision softwares for VIT'S first fully autonomous rover capable of mappling its environment and performing SLAM.</li>
+                <li>Works on OpenCV-Python, YOLO, Detectron2, Pytorch, etc for Image processing tasks like Object Detection, Localization, Segmentation, Classification,etc and Website Development.</li>
+                </ul>
+            </div>
+
+            <div class="resume-item">
+
+            <a href="https://vit.ac.in/centers/arc" role="button" rel="noreferrer" target="_blank">
+              <h4>Autonomous Research Center</h4>
+            </a>
+            
+              <h5>Software Development Head      (Jan 2023 - Present)
+              <br />Software Developer      (Aug 2022 - Jan 2023)</h5>
+
+              <a href="https://goo.gl/maps/CC88KopimCAWEmV17" role="button" rel="noreferrer" target="_blank">
+              <p><i class="bi bi-geo-alt" />
+                <em>&nbsp;&nbsp;Vellore, Tamil Nadu </em></p>
+                </a>
+              <ul>
+                <li>Researching on Computer Vision and Image processing stack for the L3 Autonomous Car of VIT using OpenCV-Python, YOLO, Detectron2, Pytorch, etc for Image processing tasks like Object Detection, Localization, Segmentation, Classification,etc and Website Development.</li>
+                </ul>
+            </div>
+
+            <div class="resume-item">
+            <a href="https://curiousjr.com/" role="button" rel="noreferrer" target="_blank">
+              <h4>CuriousJr</h4>
+            </a>
+
+              <h5>SDE Intern (Feb 2022 - July 2022)</h5>
+              
+              <p><i class="bi bi-geo-alt"></i>
+              <em>&nbsp;&nbsp;Remote</em></p>
+              <ul>
+                <li>Worked with the Software Development team to develop responsive mobile applications for the CuriousJr App.</li>
+                <li>Worked on Blockly, JQuery, JavaScript, HTML, CSS, BOOTSTRAP, git to develop block coding applications for kids. </li>
+                <li>Wrote scripts on various computer science concepts and CuriousJr applications.</li>
+              </ul>
+            </div>
+          </div>
+
+           <div class="col-lg-6">
             
             <h3 class="resume-title">Education</h3>
             
@@ -69,49 +130,11 @@ const Resume = () => {
           </div>
 
 
-
-
-          <div class="col-lg-6">
-            <h3 class="resume-title">Professional Experience</h3>
-            <div class="resume-item">
-
-            <a href="https://teamautoz.com/" role="button" rel="noreferrer" target="_blank">
-              <h4>Team AutoZ</h4>
-            </a>
-            
-              <h5>Computer Vision Engineer <br />(Dec 2020 - Aug 2022)</h5>
-              <br />
-              <h5>Head of Artificial Intelligence <br />(Aug 2022 - Present)</h5>
-
-              <a href="https://goo.gl/maps/CC88KopimCAWEmV17" role="button" rel="noreferrer" target="_blank">
-              <p><i class="bi bi-geo-alt" />
-                <em>&nbsp;&nbsp;Vellore, Tamil Nadu </em></p>
-                </a>
-              <ul>
-                <li>Works on developing computer vision softwares for VIT'S first fully autonomous rover capable of mappling its environment and performing SLAM.</li>
-                <li>Works on OpenCV-Python, YOLO, Detectron2, Pytorch, etc for Image processing tasks like Object Detection, Localization, Segmentation, Classification,etc.</li>
-                <li>Worked on the Website Development of the team from frontend to hosting using HTML, CSS, JS, BOOTSTRAP,etc.</li>
-                </ul>
-            </div>
-            <div class="resume-item">
-            <a href="https://curiousjr.com/" role="button" rel="noreferrer" target="_blank">
-              <h4>CuriousJr</h4>
-            </a>
-
-              <h5>SDE Intern <br />(Feb 2022 - July 2022)</h5>
-              
-              <p><i class="bi bi-geo-alt"></i>
-              <em>&nbsp;&nbsp;Remote</em></p>
-              <ul>
-                <li>Worked with the Software Development team to develop responsive mobile applications for the CuriousJr App.</li>
-                <li>Worked on Blockly, JQuery, JavaScript, HTML, CSS, BOOTSTRAP, git to develop block coding applications for kids. </li>
-                <li>Wrote scripts on various computer science concepts and CuriousJr applications.</li>
-              </ul>
-            </div>
-          </div>
         </div>
         <br />
 
+
+        
       </div>
     </section>
     </div>
